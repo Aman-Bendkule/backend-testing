8fab37d534c1450b3de526da13da5d81bfcc09cb
Aman Bendkule <140185913+Aman-Bendkule@users.noreply.github.com>
2025-01-10 16:20:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const cors = require('cors');
 const app = express()
 const port = 3000;
 
-app.use(cors());
+app.use(cors({ 
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    allowedHeaders: ['Content-Type', 'Authorization'] 
+}));
 
 const bcrypt = require('bcrypt')
 
